b00128f952bd25669603b2d89521ab7f698d76b6
sunysty <seokjiseon@gmail.com>
2022-02-15 19:31:05 +0900

    clicktoevent 완성

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,32 +1,31 @@
 // import './App.css';
-import styled from 'styled-components'
-import Toggle from './component/Toggle'
+import styled from 'styled-components';
+import Toggle from './component/Toggle';
 import Modal from './component/Modal';
-import Tab from './component/Tab'
-import Tag from './component/Tag'
-import AutoComplete from './component/AutoComplete'
+import Tab from './component/Tab';
+import Tag from './component/Tag';
+import AutoComplete from './component/AutoComplete';
 import ClickToEdit from './component/ClickToEdit';
 
-
 function App() {
-  return (
-    <>
-    <Container>
-      <Toggle/>
-      <Modal/>
-      <Tab/>
-      <Tag/>
-      <AutoComplete/>
-      <ClickToEdit/>
-    </Container>
-    </>
-  );
+    return (
+        <>
+            <Container>
+                <Toggle />
+                <Modal />
+                <Tab />
+                <Tag />
+                <ClickToEdit />
+                <AutoComplete />
+            </Container>
+        </>
+    );
 }
 
 export default App;
 
 const Container = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content:space-around;
-`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
--- custom-component/src/component/ClickToEdit.js
+++ custom-component/src/component/ClickToEdit.js
@@ -1,56 +1,89 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const ClickToEdit = () => {
-  const [name,setName] = useState(null)
-  const [age,setAge] = useState(null)
+    const [name, setName] = useState(null);
+    const [age, setAge] = useState(null);
+    const onChangeName = () => {
+        document.getElementById('nameId').addEventListener('focusout', (e) => {
+            setName(e.target.value);
+        });
+    };
+    const onChangeAge = () => {
+        document.getElementById('ageId').addEventListener('focusout', (e) => {
+            setAge(e.target.value);
+        });
+    };
 
-  const onChange = () => {
-    document.getElementById('nameId', 'ageId').addEventListener('focusout', e => { setName(e.target.value) }) 
-    document.getElementById('nameId', 'ageId').addEventListener('focusout', e => { setName(e.target.value) }) 
-  }
-
-  return( 
-    <>
-    <Container>
-      <Title>ClickToEdit</Title>
-      <Box className="box">
-        <Label>이름</Label>
-        <Input type='text' onChange={onChange} value={name} id='nameId'/>
-        <Label>나이</Label>
-        <Input type='number' onChange={onChange} value={age} id='ageId'/>
-      </Box>
-      <Result>이름{name} 나이{age}</Result>
-    </Container>
-    </>
-  )
-}
+    return (
+        <>
+            <Container>
+                <Title>ClickToEdit</Title>
+                <Box className='box'>
+                    <InputContainer>
+                        <Label>이름</Label>
+                        <Input
+                            type='text'
+                            onChange={onChangeName}
+                            id='nameId'
+                        />
+                    </InputContainer>
+                    <InputContainer>
+                        <Label>나이</Label>
+                        <Input
+                            type='number'
+                            onChange={onChangeAge}
+                            id='ageId'
+                        />
+                    </InputContainer>
+                </Box>
+                <Result>
+                    이름 &nbsp; {name} &emsp; 나이 &nbsp; {age}
+                </Result>
+            </Container>
+        </>
+    );
+};
 
 export default ClickToEdit;
 
 const Container = styled.div`
-  width:80vw;
-  margin:0 auto;
-  border:1px solid #ccc;
-  border-radius:10px;
-  box-sizing:border-box;
-`
+    width: 80vw;
+    height: 300px;
+    margin: 0 auto;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    box-sizing: border-box;
+`;
 
 const Title = styled.h1`
-  font-size:20px;
-  padding:10px;
-  margin:0;
-`
+    font-size: 20px;
+    padding: 10px;
+    margin: 0;
+`;
 
 const Box = styled.div`
-`
+    margin-top: 30px;
+`;
 
+const InputContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+    box-sizing: border-box;
+`;
 const Label = styled.label`
-
-`
+    padding: 5px 10px;
+`;
 const Input = styled.input`
-
-`
+    padding: 5px;
+    box-sizing: border-box;
+    text-align: center;
+`;
 
 const Result = styled.div`
-`
+    width: 100%;
+    text-align: center;
+    margin-top: 50px;
+`;
